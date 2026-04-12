d59ed20dfe3c88baa31058178c731c092aed331f
lolosaisa <lwamaitha3924@gmail.com>
2026-04-12 20:35:20 +0300

    feat: updated the routes for auth file and mechanics

--- tracking-backend/src/routes/auth.ts
+++ tracking-backend/src/routes/auth.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import { registerDriver, loginDriver, getMe } from '../controllers/auth';
+import { authMiddleware } from '../middlewares/auth';
 const router = Router();
 router.get('/', (req, res) => res.json({ message: 'Authentication  route works' }));
+
+
+router.post('/register', registerDriver);
+router.post('/login',    loginDriver);
+router.get('/me',        authMiddleware, getMe);
+
 export default router;
 
--- tracking-backend/src/routes/mechanics.ts
+++ tracking-backend/src/routes/mechanics.ts
@@ -1,11 +1,14 @@
 // src/routes/mechanics.ts
 import { Router } from 'express';
 import { get } from 'node:http';
-import { getAllMechanics, getMechanicById } from '../controllers/mechanics';
+import { getAllMechanics, getMechanicById, registerMechanic, updateMechanicLocation } from '../controllers/mechanics';
+import { register } from 'node:module';
 const router = Router();
 
 router.get('/', getAllMechanics);
 router.get('/:id', getMechanicById);
+router.post('/register', registerMechanic);
+router.patch('/:id/location', updateMechanicLocation);
 
 router.get('/', (req, res) => res.json({ message: 'mechanics route works' }));
 export default router;
